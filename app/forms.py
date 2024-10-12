33d7b7510bf54b7e6b3a34571edee6192e47bcd5
NeLeEnGiChUkI <lovelyneleen@gmail.com>
2024-10-12 11:53:11 +0300

    error handling

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,25 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, BooleanField, SelectField, TextAreaField
-from wtforms.validators import DataRequired, Length, EqualTo, Email
+from wtforms.validators import DataRequired, Length, EqualTo, Email, ValidationError
+from app.models import User
 
 class EditProfileForm(FlaskForm):
+    """Edit profile form"""
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About_me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Edit Profile')
+
+    def __init__(self, original_username, *args, **kwargs) -> None:
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data !=self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username.')
+
+
 
 class LoginForm(FlaskForm):
     """Login form"""
@@ -23,6 +37,17 @@ class RegisterForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
+    def validate_username(self, username):
+        """Validate new username"""
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+        
+    def validate_email(self, email):
+        """Validate new user email"""
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email adress.')
 
 class PostForm(FlaskForm):
     """Comment Forms"""
